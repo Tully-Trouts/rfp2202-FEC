31c65a309773763061d66e1c109ada2eaa65a682
Jerremy Stroman <jerremystroman@gmail.com>
2022-03-31 16:14:09 -0700

    Getter functions going

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,60 @@
 import React from 'react';
-import Overview from './Overview.jsx';
-import QnA from './Q&A.jsx';
-import Ratings from './Ratings.jsx';
-import RelatedItems from './RelatedItems.jsx';
+import axios from 'axios';
+import Overview from './Overview';
+import QnA from './Q&A';
+import Ratings from './Ratings';
+import RelatedItems from './RelatedItems';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      product: {},
+      reviewList: [],
+      questionsList: [],
+      outfit: [],
+      cart: [],
+    };
+    this.getProductById = this.getProductById.bind(this);
+    this.getReviewList = this.getReviewList.bind(this);
+  }
 
-    }
+  getProductById(id) {
+    axios.get(`/api/products/${id}`)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  getReviewList(productId) {
+    axios.get('/api/reviews/', {
+      params: {
+        sort: 'relevant',
+        product_id: productId,
+      },
+    })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   render() {
     return (
       <div>
+        <label htmlFor="test">Button</label>
+        <button type="button" name="test" onClick={()=>{this.getReviewList(65635)}}> TEST! </button>
         <Overview />
         <QnA />
         <Ratings />
         <RelatedItems />
       </div>
-    )
+    );
   }
 }
 
