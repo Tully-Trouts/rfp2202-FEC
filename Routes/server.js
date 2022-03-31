17dd68432e66d9101f6bfe77e8f05bd0a5084425
Jerremy Stroman <jerremystroman@gmail.com>
2022-03-31 11:34:40 -0700

    Basic server boilerplate

--- Routes/server.js
+++ Routes/server.js
@@ -1,11 +1,38 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
+
+// this is weird
+const console = require('console');
+
+const app = express();
+
+app.use(express.json());
+
+// app.set('Authorization', process.env.API_KEY);
+
+// process.env.API_URL
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
-})
+app.get('/:location', (req, res) => {
+  const apiUrl = path.join(process.env.API_URL, req.params.location);
+  console.log(apiUrl);
+  axios.get(apiUrl, {
+    headers: { Authorization: process.env.API_KEY },
+  })
+    .then((response) => {
+      console.log(response.data);
+      res.send(response.data);
+    })
+    .catch((err) => {
+      console.log('failed', err);
+    });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`App listening on port ${process.env.PORT}`);
+});
