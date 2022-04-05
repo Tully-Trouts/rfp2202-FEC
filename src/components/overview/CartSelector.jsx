a24582ee04229edfd0183f946c97b462885826dd
Jerremy Stroman <jerremystroman@gmail.com>
2022-04-05 16:36:00 -0700

    Quantity selector created

--- src/components/overview/CartSelector.jsx
+++ src/components/overview/CartSelector.jsx
@@ -1,20 +1,44 @@
 import React from 'react';
 
 var CartSelector = ({skus}) => {
-  var sizes = (skus) => {
+  const [selectedSku, setSelectedSku] = React.useState({});
+  //const [skuList, setSkuList] = React.useState([]);
+  const [quantities, setQuantities] = React.useState(0);
+
+  var getSizeOptions = (skus) => {
     return (skus ?
       Object.keys(skus).map((element) => (
-        <option key={element}>{skus[element].size}</option>
+        <option key={element} value={element}>{skus[element].size}</option>
       )) :
       <option>OUT OF STOCK</option>); // idk why the BRD WANTS TO YELL???
   };
+
+  var getQtyOptions = (qty) => {
+    const qtyList = [...Array(qty)];
+    return (qtyList.map((element, index) => (
+      <option key={index} value={index + 1}>{index + 1}</option>
+    )));
+  };
+
+  var selectSku = (event) => {
+    const sku = event.target.value;
+    console.log('selected sku:', sku);
+    setSelectedSku(sku);
+    console.log('quantity available:', skus[sku].quantity);
+    setQuantities(skus[sku].quantity);
+  };
+
   return (
     <div className="overview overview-cart-selector">
       [cart selector]
       <div className="overview sm cart-selector">
-        <select>
+        <select onChange={(e) => selectSku(e)}>
           <option defaultValue={true}>Select Size</option>
-          {sizes(skus)}
+          {getSizeOptions(skus)}
+        </select>
+        <select role="listbox">
+          <option defaultValue={true}>-</option>
+          {getQtyOptions(quantities)}
         </select>
       </div>
       <div className="overview sm cart-selector-buttons">
