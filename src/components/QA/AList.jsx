375abdadb85d7aeadc198042b96ed03a82cfcf83
Matthew Luu <mattmluu@gmail.com>
2022-04-05 23:58:05 -0700

    render only 4 Q's and 2 A's by default

--- src/components/QA/AList.jsx
+++ src/components/QA/AList.jsx
@@ -2,15 +2,20 @@ import React, {Component} from 'react';
 import Answer from './Answer';
 
 class AList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toLoad: 2,
+    };
+  }
   render() {
     const {answers} = this.props;
+    const {toLoad} = this.state;
     return (
       <div className='A_List'>
-        {
-          Object.keys(answers).map((id) =>
-            <Answer answer={answers[id]} key={id}/>
-          )
-        }
+        {Object.keys(answers).slice(0, toLoad).map((id) =>
+          <Answer answer={answers[id]} key={id}/>
+        )}
         <button>Load More Answers</button>
       </div>
     );
--- src/components/QA/QList.jsx
+++ src/components/QA/QList.jsx
@@ -2,11 +2,19 @@ import React, {Component} from 'react';
 import Question from './Question';
 
 class QList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toLoad: 4,
+    };
+  }
+
   render() {
     const {questions} = this.props;
+    const {toLoad} = this.state;
     return (
       <div className='Q_List'>
-        {questions.map((question) =>
+        {questions.slice(0, toLoad).map((question) =>
           <Question question={question} key={question.question_id} />
         )}
       </div>
