398d059fc57b1f836befbe3f690153981406e28b
Matthew Luu <mattmluu@gmail.com>
2022-04-08 15:31:32 -0700

    added Question modal pop up to show up

--- src/components/QA/QA.jsx
+++ src/components/QA/QA.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import axios from 'axios';
 import QList from './QList';
 import QASearch from './QASearch';
+import QuestionModal from './QuestionModal';
 
 class QA extends Component {
   constructor(props) {
@@ -9,13 +10,24 @@ class QA extends Component {
     this.state = {
       productId: 65631, //this.props.productId,
       questions: [],
-      search: ''
+      search: '',
+      newQuestionBody: '',
+      newQuestionNickname: '',
+      newQuestionEmail: '',
+      isQuestionModalOpen: false,
     };
 
     this.getQuestionsById = this.getQuestionsById.bind(this);
     this.liftSearch = this.liftSearch.bind(this);
     this.filter = this.filter.bind(this);
     this.liftClear = this.liftClear.bind(this);
+
+    this.addQuestionClick = this.addQuestionClick.bind(this);
+    this.closeAddQuestion = this.closeAddQuestion.bind(this);
+    this.handleNewQuestionInput = this.handleNewQuestionInput.bind(this);
+    this.handleNicknameInput = this.handleNicknameInput.bind(this);
+    this.handleEmailInput = this.handleEmailInput.bind(this);
+    this.handleNewQuestionSubmit = this.handleNewQuestionSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -79,10 +91,40 @@ class QA extends Component {
     return questions;
   }
 
+  addQuestionClick(e) {
+    e.preventDefault();
+    this.setState({isQuestionModalOpen: true});
+  }
+
+  closeAddQuestion(e) {
+    e.preventDefault();
+    this.setState({isQuestionModalOpen: false});
+  }
+
+  handleNewQuestionInput(e) {
+    e.preventDefault;
+    this.setState({newQuestionBody: e.target.value});
+  }
+
+  handleNicknameInput(e) {
+    e.preventDefault();
+    this.setState({newQuestionNickname: e.target.value});
+  }
+
+  handleEmailInput(e) {
+    e.preventDefault();
+    this.setState({newQuestionEmail: e.target.value});
+  }
+
+  handleNewQuestionSubmit(e) {
+    e.preventDefault();
+  }
+
   render() {
-    const {questions} = this.state;
-    const {liftSearch, liftClear, filter} = this;
+    const {questions, isQuestionModalOpen, newQuestionBody, newQuestionNickname, newQuestionEmail} = this.state;
+    const {liftSearch, liftClear, filter, addQuestionClick, closeAddQuestion, handleNewQuestionInput, handleNicknameInput, handleEmailInput, handleNewQuestionSubmit} = this;
     const {product} = this.props;
+
     return (
       <div className='QA'>
         <h3 className='QA_Title'>Questions and Answers</h3>
@@ -95,7 +137,29 @@ class QA extends Component {
             <QList questions={filter()} product={product}/>
           </div>
         }
-        <button className='Add_Q'>Add A Question +</button>
+
+        <button className='Add_Q' onClick={addQuestionClick}>Add A Question +</button>
+        <QuestionModal open={isQuestionModalOpen} onClose={closeAddQuestion}>
+          <form onSubmit={handleNewQuestionSubmit}>
+            <h3>Ask Your Question</h3>
+            <h4>About the {product.name} here</h4>
+            <div>
+              <label>Enter Question: </label>
+              <textarea value={newQuestionBody} placeholder='Your Question' onChange={handleNewQuestionInput} rows='10' cols='100' />
+            </div>
+            <div>
+              <label>Enter Nickname: </label>
+              <textarea value={newQuestionNickname} placeholder='Example: jackson11!' onChange={handleNicknameInput} rows='1' cols='40' />
+            </div>
+            <span>
+              <label>Enter Email: </label>
+              <textarea value={newQuestionEmail} placeholder='Example: jack@email.com' onChange={handleEmailInput} rows='1' cols='40' />
+            </span>
+            <div>
+              <button type='submit'>Submit</button>
+            </div>
+          </form>
+        </QuestionModal>
       </div>
     );
   }
