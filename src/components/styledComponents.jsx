ae75cd61d695137b1c23e438ff79fa6fa62d3e7d
Jerremy Stroman <jerremystroman@gmail.com>
2022-04-08 13:42:05 -0700

    Moving centering to CSS

--- src/components/styledComponents.jsx
+++ src/components/styledComponents.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Button = (props) => {
-  const center = props.size === 1 ? 'center' : 'normal';
   return (
     <button
       className={`styled-btn styled-btn-size-${props.size}`}
       style={{
         flex: props.size,
-        justifyContent: center
       }}
       type="button"
       role="button">
