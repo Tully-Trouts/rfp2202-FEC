490f869f19e4b8baee7982f27c8f24b241f4cf5b
Jerremy Stroman <jerremystroman@gmail.com>
2022-04-11 16:37:47 -0700

    Got carried away with the star component

--- src/components/styledComponents.jsx
+++ src/components/styledComponents.jsx
@@ -48,28 +48,35 @@ const Input = (props) => {
 };
 
 const StarReview = (props) => {
+  // Customize color with props.color
+  // Customize the number of stars in demoninator with props.max [default = 5]
+
   let marginLeft = '0%';
   if (props.value) {
     marginLeft = `${props.value}%`;
   } else if (props.stars) {
-    marginLeft = `${(props.stars / 5) * 100}%`;
+    marginLeft = `${(props.stars / (props.max || 5)) * 100}%`;
   }
 
-  // Enabling custom color
+  // custom color
   let starStyle = {};
   if (props.color) {
     starStyle.fill = props.color;
   }
 
+  var drawStars = (max) => {
+    return Array.from(Array(max), (e, i) => (
+      <svg viewBox="0 0 51 48" width="20" title="star" key={i}>
+        <path d="m 25 1 l 6 17 h 18 l -14 11 l 5 17 l -15 -10 l -15 10 l 5 -17 l -14 -11 h 18 Z"></path>
+      </svg>
+    ));
+  }
+
   return (
     <div className="star-component star-component-container">
       <div className="star-component star-component-stars" style={starStyle}>
-        <svg viewBox="0 0 576 512" width="20" title="star"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
-        <svg viewBox="0 0 576 512" width="20" title="star"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
-        <svg viewBox="0 0 576 512" width="20" title="star"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
-        <svg viewBox="0 0 576 512" width="20" title="star"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
-        <svg viewBox="0 0 576 512" width="20" title="star"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"></path></svg>
-        <div className="star-component star-component-cover"
+        {drawStars(props.max || 5)}
+       <div className="star-component star-component-cover"
           style={{marginLeft}}>
         </div>
       </div>
