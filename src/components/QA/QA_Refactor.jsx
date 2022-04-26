941f381ca80c03468ddb1011b337f36914cfee09
Matthew Luu <mattmluu@gmail.com>
2022-04-26 01:49:24 -0700

    QA boilerplate

---
+++ src/components/QA/QA_Refactor.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import Question from './Question';
+import QASearch from './QASearch';
+import QuestionModal from './QuestionModal';
+import { Button, Link } from '../styledComponents';
+
+const QA = (props) => {
+
+};
+
+export default QA;
